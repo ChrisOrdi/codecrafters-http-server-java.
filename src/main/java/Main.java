05f9d19857b32b3e7130d6f7ccb7dd89f3612dc9
ChrisOrdi <144001508+ChrisOrdi@users.noreply.github.com>
2024-06-26 08:28:51 +0200

    pass 1st stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -15,37 +15,49 @@ public class Main {
     try {
       serverSocket = new ServerSocket(4221);
       serverSocket.setReuseAddress(true);
-      clientSocket = serverSocket.accept();
-      System.out.println("Accepted new connection");
 
-      // Read the request line
-      BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-      String requestLine = in.readLine();
-      System.out.println("Request Line: " + requestLine);
+      while (true) {
+        clientSocket = serverSocket.accept();
+        System.out.println("Accepted new connection");
 
-      // Extract the URL path
-      String[] requestParts = requestLine.split(" ");
-      String urlPath = requestParts[1];
+        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        String requestLine = in.readLine();
+        System.out.println("Request Line: " + requestLine);
 
-      // Prepare the HTTP response
-      String httpResponse;
-      if ("/".equals(urlPath)) {
-        httpResponse = "HTTP/1.1 200 OK\r\n\r\n";
-      } else {
-        httpResponse = "HTTP/1.1 404 Not Found\r\n\r\n";
+        if (requestLine == null) {
+          continue;
+        }
+
+        String[] requestParts = requestLine.split(" ");
+        String urlPath = requestParts[1];
+
+        String httpResponse;
+        if (urlPath.startsWith("/echo/")) {
+          String echoString = urlPath.substring(6);
+          String responseBody = echoString;
+          String contentType = "text/plain";
+          int contentLength = responseBody.length();
+
+          httpResponse = "HTTP/1.1 200 OK\r\n" +
+                  "Content-Type: " + contentType + "\r\n" +
+                  "Content-Length: " + contentLength + "\r\n" +
+                  "\r\n" +
+                  responseBody;
+        } else if ("/".equals(urlPath)) {
+          httpResponse = "HTTP/1.1 200 OK\r\n\r\n";
+        } else {
+          httpResponse = "HTTP/1.1 404 Not Found\r\n\r\n";
+        }
+
+        OutputStream clientOutput = clientSocket.getOutputStream();
+        clientOutput.write(httpResponse.getBytes("UTF-8"));
+        clientOutput.flush();
+        clientSocket.close();
       }
-
-      // Send the HTTP response to the client
-      OutputStream clientOutput = clientSocket.getOutputStream();
-      clientOutput.write(httpResponse.getBytes("UTF-8"));
-      clientOutput.flush();
     } catch (IOException e) {
       System.out.println("IOException: " + e.getMessage());
     } finally {
       try {
-        if (clientSocket != null) {
-          clientSocket.close();
-        }
         if (serverSocket != null) {
           serverSocket.close();
         }
