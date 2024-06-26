572f36b819fdd84b891a55674ef955b6e5ade2b3
ChrisOrdi <144001508+ChrisOrdi@users.noreply.github.com>
2024-06-26 08:32:45 +0200

    pass 1st stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,55 +4,87 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
   public static void main(String[] args) {
     System.out.println("Logs from your program will appear here!");
 
     ServerSocket serverSocket = null;
-    Socket clientSocket = null;
 
     try {
       serverSocket = new ServerSocket(4221);
       serverSocket.setReuseAddress(true);
 
       while (true) {
-        clientSocket = serverSocket.accept();
-        System.out.println("Accepted new connection");
+        try (Socket clientSocket = serverSocket.accept()) {
+          System.out.println("Accepted new connection");
 
-        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        String requestLine = in.readLine();
-        System.out.println("Request Line: " + requestLine);
+          BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+          String requestLine = in.readLine();
+          System.out.println("Request Line: " + requestLine);
 
-        if (requestLine == null) {
-          continue;
+          if (requestLine == null) {
+            continue;
+          }
+
+          // Read headers
+          Map<String, String> headers = new HashMap<>();
+          String headerLine;
+          while (!(headerLine = in.readLine()).isEmpty()) {
+            String[] headerParts = headerLine.split(": ", 2);
+            if (headerParts.length == 2) {
+              headers.put(headerParts[0].toLowerCase(), headerParts[1]);
+            }
+          }
+
+          String[] requestParts = requestLine.split(" ");
+          String method = requestParts[0];
+          String urlPath = requestParts[1];
+
+          String httpResponse;
+          if (method.equals("GET")) {
+            if (urlPath.equals("/")) {
+              httpResponse = "HTTP/1.1 200 OK\r\n\r\n";
+            } else if (urlPath.startsWith("/echo/")) {
+              String message = urlPath.substring(6);
+              String responseBody = message;
+              String contentType = "text/plain";
+              int contentLength = responseBody.length();
+
+              httpResponse = "HTTP/1.1 200 OK\r\n" +
+                      "Content-Type: " + contentType + "\r\n" +
+                      "Content-Length: " + contentLength + "\r\n" +
+                      "\r\n" +
+                      responseBody;
+            } else if (urlPath.equals("/user-agent")) {
+              String userAgent = headers.get("user-agent");
+              if (userAgent == null) {
+                userAgent = "";
+              }
+              String responseBody = userAgent;
+              String contentType = "text/plain";
+              int contentLength = responseBody.length();
+
+              httpResponse = "HTTP/1.1 200 OK\r\n" +
+                      "Content-Type: " + contentType + "\r\n" +
+                      "Content-Length: " + contentLength + "\r\n" +
+                      "\r\n" +
+                      responseBody;
+            } else {
+              httpResponse = "HTTP/1.1 404 Not Found\r\n\r\n";
+            }
+          } else {
+            httpResponse = "HTTP/1.1 405 Method Not Allowed\r\n\r\n";
+          }
+
+          OutputStream clientOutput = clientSocket.getOutputStream();
+          clientOutput.write(httpResponse.getBytes("UTF-8"));
+          clientOutput.flush();
+        } catch (IOException e) {
+          System.out.println("IOException: " + e.getMessage());
         }
-
-        String[] requestParts = requestLine.split(" ");
-        String urlPath = requestParts[1];
-
-        String httpResponse;
-        if (urlPath.startsWith("/echo/")) {
-          String echoString = urlPath.substring(6);
-          String responseBody = echoString;
-          String contentType = "text/plain";
-          int contentLength = responseBody.length();
-
-          httpResponse = "HTTP/1.1 200 OK\r\n" +
-                  "Content-Type: " + contentType + "\r\n" +
-                  "Content-Length: " + contentLength + "\r\n" +
-                  "\r\n" +
-                  responseBody;
-        } else if ("/".equals(urlPath)) {
-          httpResponse = "HTTP/1.1 200 OK\r\n\r\n";
-        } else {
-          httpResponse = "HTTP/1.1 404 Not Found\r\n\r\n";
-        }
-
-        OutputStream clientOutput = clientSocket.getOutputStream();
-        clientOutput.write(httpResponse.getBytes("UTF-8"));
-        clientOutput.flush();
-        clientSocket.close();
       }
     } catch (IOException e) {
       System.out.println("IOException: " + e.getMessage());
